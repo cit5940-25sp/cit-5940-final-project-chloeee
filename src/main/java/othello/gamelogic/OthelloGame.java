3d0d06a5b45ba19acfb5b919528d0e84b2338564
pennmyang <mickyang@seas.upenn.edu>
2025-04-03 09:26:27 -0400

    takeSpace() and takeSpaces() done

--- src/main/java/othello/gamelogic/OthelloGame.java
+++ src/main/java/othello/gamelogic/OthelloGame.java
@@ -64,7 +64,41 @@ public class OthelloGame {
      * @param x the x-coordinate of the space to claim
      * @param y the y-coordinate of the space to claim
      */
-    public void takeSpace(Player actingPlayer, Player opponent, int x, int y) {}
+    public void takeSpace(Player actingPlayer, Player opponent, int x, int y) {
+        // check x and y are valid inputs
+        if (x < 0 || x >= GAME_BOARD_SIZE || y < 0 || y >= GAME_BOARD_SIZE) {
+            return;
+        }
+        // No need - check actingPlayer, opponent valid
+        // Construct BoardSpace for this x-y space
+        BoardSpace boardSpace = board[x][y];
+
+        // First check for current player bc if already owned by them, do nothing
+        if (boardSpace.getType() == actingPlayer.getColor()) {
+            return;
+        }
+        // If NOT already owned by current player, it could be empty or owned by opponent
+        // either way this might change, so need to record current state before changing
+        BoardSpace.SpaceType previousType = boardSpace.getType(); // type is an ENUM represents STATE: empty, black, white
+
+        // Flow of game in GameController and getAvailableMoves() means being able call
+        // takeSpace on a board space owned by opponent is legal and results in a 'win' flip of opponent's color
+
+        // Update the lists based on previous state
+        // Remove this x-y space from opponent's list of board spaces
+        if (previousType == opponent.getColor()) {
+            opponent.getPlayerOwnedSpacesSpaces().remove(boardSpace);
+        }
+
+        // Change the space type (COLOR) to current player's color (already checked this is possible)
+        boardSpace.setType(actingPlayer.getColor());
+
+        // Add this x-y space to acting player's list of board spaces
+        actingPlayer.getPlayerOwnedSpacesSpaces().add(boardSpace);
+
+        // check if space already taken by actingPlayer (which has getter of List<BoardSpace> playerOwnedSpaces )
+        actingPlayer.getPlayerOwnedSpacesSpaces();
+    }
 
     /**
      * PART 1
@@ -76,8 +110,37 @@ public class OthelloGame {
      * @param availableMoves map of the available moves, that maps destination to list of origins
      * @param selectedDestination the specific destination that a HUMAN player selected
      */
-    public void takeSpaces(Player actingPlayer, Player opponent, Map<BoardSpace, List<BoardSpace>> availableMoves, BoardSpace selectedDestination) {}
+    // Whereas getAvailableMoves identifies where a player can legally place a piece and which existing pieces form valid "sandwich" lines
+    // takeSpaces actually executes a move by placing a piece and flipping all sandwiched pieces
+    public void takeSpaces(Player actingPlayer, Player opponent, Map<BoardSpace, List<BoardSpace>> availableMoves, BoardSpace selectedDestination) {
+        // Claim the specified space (selectedDestination) for the current player; rest of method only activates after this is DONE
+        takeSpace(actingPlayer, opponent, selectedDestination.getX(), selectedDestination.getY());
 
+        // availablesMoves is a map with a destination BoardSpace mapped to a List of origin BoardSpaces.
+        // Keys: Valid destination spaces where a player can place a new piece
+        // Values: Lists of "origin" spaces (those player already owns) that form valid sandwiching lines with that destination
+        // Get all origins (values: List) for this selected destination (keys) from Map <BoardSpace, List<BoardSpace>>
+        List<BoardSpace> origins = availableMoves.get(selectedDestination);
+
+        // For every origin space in List, flip all pieces between origin and destination
+        for (BoardSpace origin : origins) {
+            // Indicate -1, 0, or 1 for the direction to take by comparing x-y coordinates of selected and origin spaces
+            int dx = Integer.compare(selectedDestination.getX() - origin.getX(), 0);
+            int dy = Integer.compare(selectedDestination.getY() - origin.getY(), 0);
+
+            // Start from the space NEXT to origin (which is already occupied, so you don't start from origin itself)
+            int x = origin.getX() + dx;
+            int y = origin.getY() + dy;
+
+            // Because availableMoves map already ensured every piece in that line between origin and destination is flippable
+            // We can keep calling takeSpace on every space until we get to destination x-y
+            while (x != selectedDestination.getX() || y != selectedDestination.getY()) {
+                takeSpace(actingPlayer, opponent, x, y);
+                x += dx;
+                y += dy;
+            }
+        }
+    }
     /**
      * PART 2
      * TODO: Implement this method
